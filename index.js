fc701eb616c7eb4f0e851b4bfc27e5e564156b18
Metano Chavana <metanochava@gmail.com>
2026-01-09 17:41:28 +0100

    release: v1.0.4 - Acerto

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-export * from './stores/auth.js'
-export * from './stores/settings.js'
-export * from './boot/axios.js'
+export * from './stores/AuthStore.js'
+export * from './stores/SettingsStore.js'
+
 
 
 import MainLayout from './layouts/MainLayout.vue'
 import AuthLayout from './layouts/AuthLayout.vue'
+
 export { MainLayout, AuthLayout}
 
