4ac669941aeb67cc6323ec4caaa0c3f3fcb0da75
Metano Chavana <metanochava@gmail.com>
2026-02-02 00:47:46 +0100

    release: v1.0.93 - rotas

--- router/restRoutes.js
+++ router/restRoutes.js
@@ -1,29 +1,25 @@
 import { tdc } from '../boot/app'
 
-export const restRoutes = [
-  {
-    path: '/',
-    component: () => import('../layouts/MainLayout.vue'),
-    children: { 
-      path: '/Teste', 
-      name: 'list_test', 
-      component: () => import('../components/MyTest.vue'), 
-      meta: { title: tdc('Lista de') + ' ' + tdc('Tests'), 
+export let restRoutes = [
+  { 
+    path: '/Teste', 
+    name: 'list_test', 
+    component: () => import('../components/MyTest.vue'), 
+    meta: { 
+      title: tdc('Lista de') + ' ' + tdc('Tests'), 
       requiresAuth: true, 
-      requiredRole: 'list_test' } 
-    },
+      requiredRole: 'list_test' 
+    } 
   },
-  {
-    path: '/',
-    component: () => import('../layouts/MainLayout.vue'),
-    children: { 
-      path: '/view_scaffold', 
-      name: 'view_scaffold', 
-      component: () => import('../pages/commands/ScaffoldPage.vue'), 
-      meta: { title: tdc('Vista de') + ' ' + tdc('Scaffold'), 
+  { 
+    path: '/view_scaffold', 
+    name: 'view_scaffold', 
+    component: () => import('../pages/commands/ScaffoldPage.vue'), 
+    meta: { 
+      title: tdc('Vista de') + ' ' + tdc('Scaffold'), 
       requiresAuth: true, 
-      requiredRole: 'view_scaffold' } 
-    },
-  },
+      requiredRole: 'view_scaffold'
+    } 
+  }
 ]
 
