89ece5b7acac4374b087f69766134660e14c3f8a
Metano Chavana <metanochava@gmail.com>
2026-02-02 16:27:16 +0100

    release: v1.0.114 - jjh

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ export * from './stores/SettingsStore.js'
 export * from './router/restRoutes.js'
 export * from './router/authRoutes.js'
 export * from './boot/app.js'
+export * from './boot/api.js'
+export * from './boot/storage.js'
 
 
 
