5c3480504e3e0d3c876542575aefe96e5ff615ca
Metano Chavana <metanochava@gmail.com>
2026-02-04 13:21:06 +0100

    release: v1.0.133 - df

--- pages/commands/ScaffoldPage.vue
+++ pages/commands/ScaffoldPage.vue
@@ -293,7 +293,7 @@ watch(()=>form.value.modulo, async m=>{
 /* LOAD FIELDS SE EXISTIR */
 async function nextStep(){
   if(step.value===2 && modelExists.value){
-    const {data} = await HTTPAuth.get(`/saas/modulos/${form.value.modulo}/${form.value.modelo}/`)
+    const {data} = await HTTPAuth.get(`/saas/modulos/${form.value.modulo}/${form.value.modelo}/schema/`)
     form.value.fields = data.fields.map(f=>({ id:Date.now()+Math.random(), ...f }))
   }
   step.value++
