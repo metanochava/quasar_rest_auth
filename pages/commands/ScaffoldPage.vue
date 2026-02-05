f0fb3d34196d10454d991d192c78dcc0bcfe41c5
Metano Chavana <metanochava@gmail.com>
2026-02-05 10:18:45 +0100

    release: v1.0.141 - kj

--- pages/commands/ScaffoldPage.vue
+++ pages/commands/ScaffoldPage.vue
@@ -1,10 +1,11 @@
 <template>
   <q-page padding>
 
-    <!-- ================= HEADER ================= -->
+    <!-- HEADER -->
     <div class="text-h5 q-mb-lg">
       🔧 {{ tdc('Scaffold Wizard') }}
     </div>
+
 
     <!-- ================= PERMISSION ================= -->
     <q-banner
@@ -12,16 +13,22 @@
       class="bg-warning text-black"
       rounded
     >
-      {{ tdc('You do not have permission to access this feature') }}
+      {{ tdc('You do not have permission') }}
     </q-banner>
 
-    <!-- ================= STEPPER ================= -->
-    <q-stepper v-else v-model="step" flat animated>
 
-      <!-- ================================================= -->
+    <!-- ================= STEPPER ================= -->
+    <q-stepper
+      v-else
+      v-model="step"
+      flat
+      animated
+    >
+
+      <!-- ========================================= -->
       <!-- STEP 1 MODULE -->
-      <!-- ================================================= -->
-      <q-step :name="1" :title="tdc('Module')" icon="folder">
+      <!-- ========================================= -->
+      <q-step :name="1" icon="folder" :title="tdc('Module')">
 
         <q-select
           v-model="form.modulo"
@@ -37,10 +44,11 @@
       </q-step>
 
 
-      <!-- ================================================= -->
+
+      <!-- ========================================= -->
       <!-- STEP 2 MODEL -->
-      <!-- ================================================= -->
-      <q-step :name="2" :title="tdc('Model')" icon="schema">
+      <!-- ========================================= -->
+      <q-step :name="2" icon="schema" :title="tdc('Model')">
 
         <q-input
           v-model="form.modelo"
@@ -55,7 +63,8 @@
         <q-chip
           v-for="m in existingModels"
           :key="m"
-          dense outline
+          dense
+          outline
           class="q-mr-xs q-mt-xs"
         >
           {{ m }}
@@ -64,144 +73,112 @@
       </q-step>
 
 
-      <!-- ================================================= -->
-      <!-- STEP 3 FIELDS -->
-      <!-- ================================================= -->
-      <q-step :name="3" :title="tdc('Fields')" icon="list">
 
-        <div
-          v-for="(f,i) in form.fields"
-          :key="f.id"
-          class="q-mb-md bordered q-pa-sm rounded-borders bg-grey-1"
+      <!-- ========================================= -->
+      <!-- STEP 3 FIELDS -->
+      <!-- ========================================= -->
+      <q-step :name="3" icon="list" :title="tdc('Fields')">
+
+        <!-- TABLE STYLE (excel-like) -->
+        <q-table
+          flat
+          dense
+          :rows="form.fields"
+          :columns="columns"
+          row-key="id"
+          hide-pagination
         >
 
-          <!-- ================= BASIC ROW ================= -->
-          <div class="row q-col-gutter-sm items-center">
+          <!-- NAME -->
+          <template #body-cell-name="p">
+            <q-input dense v-model="p.row.name"/>
+          </template>
 
-            <q-btn flat dense icon="arrow_upward" @click="moveUp(i)" />
-            <q-btn flat dense icon="arrow_downward" @click="moveDown(i)" />
-
-            <div class="col-3">
-              <q-input v-model="f.name" :label="tdc('Name')" outlined/>
-            </div>
-
-            <div class="col-3">
-              <q-select
-                v-model="f.type"
-                :options="typeOptions"
-                option-label="label"
-                option-value="value"
-                emit-value
-                map-options
-                outlined
-                :label="tdc('Type')"
-              />
-            </div>
-
-            <q-btn
-              v-if="User.can('delete_scaffold')"
-              flat dense icon="delete" color="negative"
-              @click="removeField(i)"
+          <!-- TYPE -->
+          <template #body-cell-type="p">
+            <q-select dense
+              v-model="p.row.type"
+              :options="typeOptions"
+              emit-value map-options
             />
-          </div>
+          </template>
 
+          <!-- REQUIRED -->
+          <template #body-cell-required="p">
+            <q-toggle v-model="p.row.required"/>
+          </template>
 
-          <!-- ================= OPTIONS ================= -->
-          <div class="row q-col-gutter-sm q-mt-sm">
+          <!-- UNIQUE -->
+          <template #body-cell-unique="p">
+            <q-toggle v-model="p.row.unique"/>
+          </template>
 
-            <q-checkbox v-model="f.required" :label="tdc('Required')" />
-            <q-checkbox v-model="f.unique" :label="tdc('Unique')" />
-
-            <div class="col-2">
-              <q-input v-model="f.default" :label="tdc('Default')" outlined/>
+          <!-- LENGTH -->
+          <template #body-cell-length="p">
+            <div class="row">
+              <q-input dense style="width:60px" v-model="p.row.min_length" placeholder="min"/>
+              <q-input dense style="width:60px" v-model="p.row.max_length" placeholder="max"/>
             </div>
+          </template>
 
-            <div class="col-2">
-              <q-input v-model="f.verbose_name" :label="tdc('Verbose name')" outlined/>
+          <!-- NUMERIC -->
+          <template #body-cell-minmax="p">
+            <div class="row">
+              <q-input dense style="width:60px" v-model="p.row.min"/>
+              <q-input dense style="width:60px" v-model="p.row.max"/>
             </div>
+          </template>
 
-            <div class="col-3">
-              <q-input v-model="f.help_text" :label="tdc('Help text')" outlined/>
-            </div>
-
-          </div>
-
-
-          <!-- ================= RELATION ================= -->
-          <div v-if="isRelation(f.type)" class="row q-col-gutter-sm q-mt-sm">
-
-            <div class="col-3">
-              <q-select
-                v-model="f.relModule"
+          <!-- RELATION -->
+          <template #body-cell-relation="p">
+            <div v-if="isRelation(p.row.type)" class="row">
+              <q-select dense
+                v-model="p.row.relModule"
                 :options="apps"
                 option-label="name"
                 option-value="name"
-                emit-value
-                map-options
-                outlined
-                :label="tdc('Module')"
-                @update:model-value="loadRelationModels(f)"
+                @update:model-value="loadRelationModels(p.row)"
+              />
+              <q-select dense
+                v-model="p.row.relation"
+                :options="p.row.relModels"
+              />
+              <q-select dense
+                v-model="p.row.on_delete"
+                :options="onDeleteOptions"
               />
             </div>
+          </template>
 
-            <div class="col-3">
-              <q-select
-                v-model="f.relation"
-                :options="f.relModels"
-                outlined
-                :label="tdc('Model')"
-              />
-            </div>
-
-          </div>
-
-
-          <!-- ================= FILE OPTIONS ================= -->
-          <div v-if="isFile(f.type)" class="row q-col-gutter-sm q-mt-sm">
-
-            <div class="col-3">
+          <!-- FILE -->
+          <template #body-cell-file="p">
+            <div v-if="isFile(p.row.type)" class="row">
+              <q-toggle v-model="p.row.customUpload"/>
               <q-input
-                v-model="f.upload_to"
-                outlined
-                :label="tdc('Upload path')"
-                placeholder="uploads/model/"
+                v-if="p.row.customUpload"
+                dense
+                v-model="p.row.upload_to"
+                placeholder="uploads/"
               />
             </div>
+          </template>
 
-          </div>
+          <!-- ACTIONS -->
+          <template #body-cell-actions="p">
+            <q-btn dense flat icon="arrow_upward" @click="moveUp(p.pageIndex)"/>
+            <q-btn dense flat icon="arrow_downward" @click="moveDown(p.pageIndex)"/>
+            <q-btn
+              v-if="User.can('delete_scaffold')"
+              dense flat color="negative" icon="delete"
+              @click="removeField(p.pageIndex)"
+            />
+          </template>
 
-
-          <!-- ================= NUMBER OPTIONS ================= -->
-          <div v-if="isNumeric(f.type)" class="row q-col-gutter-sm q-mt-sm">
-
-            <div class="col-2">
-              <q-input v-model="f.min" type="number" outlined :label="tdc('Min')"/>
-            </div>
-
-            <div class="col-2">
-              <q-input v-model="f.max" type="number" outlined :label="tdc('Max')"/>
-            </div>
-
-          </div>
-
-
-          <!-- ================= DECIMAL ================= -->
-          <div v-if="f.type==='DecimalField'" class="row q-col-gutter-sm q-mt-sm">
-
-            <div class="col-2">
-              <q-input v-model="f.max_digits" type="number" outlined label="max_digits"/>
-            </div>
-
-            <div class="col-2">
-              <q-input v-model="f.decimal_places" type="number" outlined label="decimal_places"/>
-            </div>
-
-          </div>
-
-        </div>
+        </q-table>
 
 
         <q-btn
+          v-if="User.can('change_scaffold')"
           icon="add"
           outlined
           class="q-mt-md"
@@ -212,38 +189,47 @@
       </q-step>
 
 
-      <!-- ================================================= -->
-      <!-- STEP 4 PREVIEW -->
-      <!-- ================================================= -->
-      <q-step :name="4" :title="tdc('Preview')" icon="code">
 
-        <pre class="bg-grey-10 text-white q-pa-md">{{ preview }}</pre>
+      <!-- ========================================= -->
+      <!-- STEP 4 PREVIEW -->
+      <!-- ========================================= -->
+      <q-step :name="4" icon="code" :title="tdc('Preview')">
+
+        <q-btn
+          icon="refresh"
+          outline
+          class="q-mb-md"
+          :label="tdc('Generate preview')"
+          @click="generatePreview"
+        />
+
+        <pre style="max-height:500px;overflow:auto">{{ preview.model }}</pre>
 
       </q-step>
 
 
-      <!-- ================================================= -->
+
+      <!-- ========================================= -->
       <!-- NAV -->
-      <!-- ================================================= -->
+      <!-- ========================================= -->
       <template #navigation>
 
-        <q-btn flat :label="tdc('Back')" @click="step--"/>
+        <q-btn flat :label="tdc('Back')" @click="step--" :disable="step===1"/>
 
-        <q-btn v-if="step<4" color="primary" :label="tdc('Next')" @click="nextStep"/>
+        <q-btn
+          v-if="step<4"
+          color="primary"
+          :label="tdc('Next')"
+          @click="nextStep"
+        />
 
         <q-btn
           v-else
           color="positive"
+          icon="rocket_launch"
           :label="modelExists ? tdc('Update') : tdc('Create')"
+          :loading="loading"
           @click="submit"
-        />
-
-        <q-btn
-          v-if="modelExists && User.can('delete_scaffold')"
-          color="negative"
-          flat
-          :label="tdc('Delete')"
-          @click="deleteModel"
         />
 
       </template>
@@ -257,35 +243,53 @@
 
 <script setup>
 import { ref, computed, watch, onMounted } from 'vue'
-import { HTTPAuth, tdc, UserStore } from './../../index'
 import { Notify } from 'quasar'
+import { HTTPAuth, tdc, UserStore } from './../../index'
 
 const User = UserStore()
 
 const step = ref(1)
+const loading = ref(false)
+
 const apps = ref([])
 const existingModels = ref([])
+
+const preview = ref({ model:'' })
 
 const form = ref({
   modulo:'',
   modelo:'',
-  fields:[]
+  fields:[],
+  permissions:[]
 })
 
-const preview = ref('')
+/* ================= TABLE COLUMNS ================= */
+const columns = [
+  {name:'name',label:'Name',field:'name'},
+  {name:'type',label:'Type',field:'type'},
+  {name:'required',label:'Req',field:'required'},
+  {name:'unique',label:'Uniq',field:'unique'},
+  {name:'length',label:'Len',field:'len'},
+  {name:'minmax',label:'Min/Max',field:'mm'},
+  {name:'relation',label:'Relation',field:'rel'},
+  {name:'file',label:'Upload',field:'file'},
+  {name:'actions',label:'',field:'a'}
+]
 
 
 /* ================= TYPES ================= */
 const rawTypes = [
-  'CharField','TextField','IntegerField','DecimalField','BooleanField',
-  'DateField','DateTimeField','FloatField',
-  'ForeignKey','OneToOneField','ManyToManyField',
-  'FileField','ImageField','JSONField'
+'CharField','TextField','SlugField','EmailField','URLField','UUIDField',
+'IntegerField','BigIntegerField','SmallIntegerField','PositiveIntegerField',
+'PositiveSmallIntegerField','FloatField','DecimalField',
+'BooleanField','DateField','DateTimeField','TimeField','DurationField',
+'FileField','ImageField','JSONField',
+'ForeignKey','OneToOneField','ManyToManyField'
 ]
 
-const typeOptions = computed(() =>
-  rawTypes.map(t=>({label:tdc(t),value:t}))
-)
+const typeOptions = computed(() => rawTypes.map(t=>({label:t,value:t})))
+
+const onDeleteOptions = ['CASCADE','PROTECT','SET_NULL','DO_NOTHING','RESTRICT']
 
 const modelExists = computed(() =>
   existingModels.value.includes(form.value.modelo)
@@ -293,108 +297,62 @@ const modelExists = computed(() =>
 
 
 /* ================= HELPERS ================= */
-function isRelation(t){
-  return ['ForeignKey','OneToOneField','ManyToManyField'].includes(t)
-}
-
-function isFile(t){
-  return ['FileField','ImageField'].includes(t)
-}
-
-function isNumeric(t){
-  return ['IntegerField','FloatField','DecimalField'].includes(t)
-}
-
 function addField(){
-  form.value.fields.push({ id:Date.now(), required:true })
+  form.value.fields.push({
+    id:Date.now(),
+    name:'',
+    type:'CharField',
+    required:true,
+    unique:false,
+    relModels:[]
+  })
 }
 
-function removeField(i){
-  form.value.fields.splice(i,1)
-}
+function removeField(i){ form.value.fields.splice(i,1) }
+function moveUp(i){ if(i>0) [form.value.fields[i-1],form.value.fields[i]]=[form.value.fields[i],form.value.fields[i-1]] }
+function moveDown(i){ if(i<form.value.fields.length-1) [form.value.fields[i+1],form.value.fields[i]]=[form.value.fields[i],form.value.fields[i+1]] }
 
-function moveUp(i){
-  const a=form.value.fields
-  ;[a[i-1],a[i]]=[a[i],a[i-1]]
-}
-
-function moveDown(i){
-  const a=form.value.fields
-  ;[a[i+1],a[i]]=[a[i],a[i+1]]
-}
+function isRelation(t){ return ['ForeignKey','OneToOneField','ManyToManyField'].includes(t) }
+function isFile(t){ return ['FileField','ImageField'].includes(t) }
 
 
 /* ================= API ================= */
 async function loadApps(){
   const {data}=await HTTPAuth.get('/saas/modulos/')
-  apps.value=data.apps||[]
+  apps.value=data.apps
 }
 
 watch(()=>form.value.modulo, async m=>{
   if(!m) return
   const {data}=await HTTPAuth.get(`/saas/modulos/${m}/`)
-  existingModels.value=data.models||[]
+  existingModels.value=data.models
 })
 
-async function nextStep(){
+
+async function loadRelationModels(field){
+  const {data}=await HTTPAuth.get(`/saas/modulos/${field.relModule}/`)
+  field.relModels=data.models
+}
+
+async function generatePreview(){
+  const {data}=await HTTPAuth.post('/saas/scaffold/preview/',form.value)
+  preview.value=data
+}
+
+async function submit(){
+  loading.value=true
+  await HTTPAuth.post('/saas/scaffold/',form.value)
+  loading.value=false
+  Notify.create({type:'positive',message:'Saved'})
+}
+
+function nextStep(){
   if(step.value===2 && modelExists.value){
-    const {data}=await HTTPAuth.get(`/saas/modulos/${form.value.modulo}/${form.value.modelo}/schema/`)
-    form.value.fields=data.fields
+    HTTPAuth.get(`/saas/modulos/${form.value.modulo}/${form.value.modelo}/schema/`)
+      .then(({data})=> form.value.fields=data.fields)
   }
   step.value++
 }
 
-async function loadRelationModels(f){
-  const {data}=await HTTPAuth.get(`/saas/modulos/${f.relModule}/`)
-  f.relModels=data.models
-}
-
-
-/* ================= PREVIEW CODE ================= */
-watch(form, ()=>{
-  preview.value = generateModelCode()
-},{deep:true})
-
-
-function generateModelCode(){
-
-  const fields = form.value.fields.map(f=>{
-    let opts=[]
-
-    if(!f.required) opts.push('blank=True')
-    if(!f.required) opts.push('null=True')
-    if(f.unique) opts.push('unique=True')
-    if(f.default) opts.push(`default=${JSON.stringify(f.default)}`)
-    if(f.upload_to) opts.push(`upload_to="${f.upload_to}"`)
-    if(f.max_digits) opts.push(`max_digits=${f.max_digits}`)
-    if(f.decimal_places) opts.push(`decimal_places=${f.decimal_places}`)
-
-    return `    ${f.name} = models.${f.type}(${opts.join(', ')})`
-  })
-
-  return `
-class ${form.value.modelo}(models.Model):
-
-${fields.join('\n')}
-
-    class Meta:
-        verbose_name = "${form.value.modelo}"
-`
-}
-
-
-/* ================= SUBMIT ================= */
-async function submit(){
-  const {data}=await HTTPAuth.post('/saas/scaffold/', form.value)
-  Notify.create({ type:'positive', message:data.alert_success })
-}
-
-async function deleteModel(){
-  await HTTPAuth.delete(`/saas/modulos/${form.value.modulo}/${form.value.modelo}/`)
-  Notify.create({ type:'negative', message:tdc('Deleted') })
-}
-
-
-/* ================= INIT ================= */
 onMounted(loadApps)
 </script>
