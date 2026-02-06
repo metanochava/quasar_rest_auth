8f3c99cedb8bfd623034e77f3662a97cca3c7fab
Metano Chavana <metanochava@gmail.com>
2026-02-07 00:12:48 +0100

    release: v1.0.172 - ghhj

--- boot/base.js
+++ boot/base.js
@@ -11,23 +11,6 @@ export const tdc = (texto = '') => {
   return store.TraducaoMap[chave] || texto
 }
 
-let result = false
-export const localStorageSetItem = (key, value) => {
-  Object.keys(localStorage).forEach(function (keyLocal) {
-    if (key === decrypt(keyLocal)) {
-      localStorage.removeItem(keyLocal)
-    }
-  })
-  localStorage.setItem(encrypt(key), (value))
-}
-
-export const pegaDominio = function () {
-  let pagelocalurl = location.href // pega endereço que esta no navegador
-  pagelocalurl = pagelocalurl.split('/') // quebra o endeço de acordo com a / (barra)
-  const dominiourl = pagelocalurl[0] + '//' + pagelocalurl[2]
-  return dominiourl // retorna a parte www.endereco.com.brs@
-}
-
 
 function captura (texto = '') {
   return texto.match(/\s*%-\s*[\w\s-]+\s*-%\s*/g) || []
@@ -98,6 +81,15 @@ function base64url (source) {
   return encodedSource
 }
 
+
+export const pegaDominio = function () {
+  let pagelocalurl = location.href // pega endereço que esta no navegador
+  pagelocalurl = pagelocalurl.split('/') // quebra o endeço de acordo com a / (barra)
+  const dominiourl = pagelocalurl[0] + '//' + pagelocalurl[2]
+  return dominiourl // retorna a parte www.endereco.com.brs@
+}
+
+
 export const createToken = (data, secret = 'se') => {
   // const base64url = require('base64url');
   const CryptoJS = require('crypto-js')
@@ -122,68 +114,6 @@ export const createToken = (data, secret = 'se') => {
   return signedToken
 }
 
-export const setCookie = (cname, cvalue, exdays) => {
-  const d = new Date()
-  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000))
-  const expires = 'expires=' + d.toUTCString()
-  document.cookie = cname + '=' + cvalue + ';' + expires + ';path=/'
-}
 
-export const setStorage = (_type, _cname, _cvalue, _exdays) => {
-  if (_type === 'l') {
-    localStorage.setItem(_cname, _cvalue)
-  }
-
-  if (_type === 'c') {
-    setCookie(_cname, _cvalue, _exdays)
-  }
-}
-
-export const getCookie = (cname, _help = 0) => {
-  const name = cname + '='
-  const ca = document.cookie.split(';')
-  for (let i = 0; i < ca.length; i++) {
-    let c = ca[i]
-    while (c.charAt(0) === ' ') {
-      c = c.substring(1)
-    }
-    if (c.indexOf(name) === 0) {
-      return c.substring(name.length, c.length)
-    }
-  }
-  if (_help === 0) {
-    return ''
-  } else {
-    return null
-  }
-}
-
-export const getStorage = (_type, _cname, _help = 0) => {
-  let result = null
-  if (_type === 'l') {
-    result = localStorage.getItem(_cname)
-  }
-
-  if (_type === 'c') {
-    if (_help === 0) {
-      result = getCookie(_cname)
-    } else {
-      result = getCookie(_cname, _help)
-    }
-  }
-  return result
-}
-
-export const deleteStorage = (_type, _cname) => {
-  let result = null
-  if (_type === 'l') {
-    result = localStorage.removeItem(_cname)
-  }
-
-  if (_type === 'c') {
-    setCookie(_cname, null, 0)
-  }
-  return result
-}
 
 
