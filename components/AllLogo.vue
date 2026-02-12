7e6f12adbd0fe6f932a3666bac62d89a063204cc
Metano Chavana <metanochava@gmail.com>
2026-02-12 22:05:06 +0100

    release: v1.0.233 - jgh

--- components/AllLogo.vue
+++ components/AllLogo.vue
@@ -1,99 +1,102 @@
-
 <template>
-  <div class=" text-center col-md-6">
-    <img v-if="User.Entidade" :src="User.Entidade?.logo?.url"   class="" style="width:40%" align="center" />
-    <img v-else-if="this.$route.params.entidade_id" :src="User.Entidade?.logo?.url"   class="" style="width:40%" align="center" />
-    <img v-else :src="User.TipoEntidade?.icon?.url"   class="" style="width:40%" align="center" />
-    <br/>
-   <label v-if="this.$route.params.entidade_id" class="text-grey-6 text-h4"> {{st(User.Entidade?.nome)}}</label>
-   <label v-else class="text-grey-6 text-h4"> {{st(User.TipoEntidade?.nome)}}</label>
+  <div class="text-center col-md-6">
+
+    <!-- LOADING -->
+    <div v-if="loading" class="q-pa-md">
+      <q-spinner size="40px" />
+    </div>
+
+    <!-- LOGO -->
+    <img
+      v-else-if="User.Entidade?.logo?.url"
+      :src="User.Entidade.logo.url"
+      style="width:40%"
+    />
+
+    <!-- FALLBACK TIPO ENTIDADE -->
+    <img
+      v-else-if="User.TipoEntidade?.icon?.url"
+      :src="User.TipoEntidade.icon.url"
+      style="width:40%"
+    />
+
+    <!-- FALLBACK FINAL -->
+    <img
+      v-else
+      src="https://placehold.co/400x300/png"
+      style="width:40%"
+    />
+
+    <br />
+
+    <!-- NOME -->
+    <label
+      v-if="User.Entidade?.nome"
+      class="text-grey-6 text-h4"
+    >
+      {{ st(User.Entidade.nome) }}
+    </label>
+
+    <label
+      v-else
+      class="text-grey-6 text-h4"
+    >
+      {{ st(User.TipoEntidade?.nome) }}
+    </label>
+
   </div>
 </template>
 
 <script setup>
-import { HTTPClient, url} from './../boot/api'
-import { UserStore, } from './../stores/AuthStore'
+import { ref, watch, onMounted } from 'vue'
+import { useRoute } from 'vue-router'
+import { HTTPClient, url } from './../boot/api'
+import { UserStore } from './../stores/AuthStore'
 
 const User = UserStore()
+const route = useRoute()
 
-</script>
+const loading = ref(false)
 
-<script >
-import { HTTPClient, apiUrl } from 'src/boot/axios'
-import { useStore } from 'vuex'
-import { appName, appLogo, entidadeName, entidadeLogo, setStorage, getStorage, st } from 'src/js/appMytech'
-import { defineComponent, computed } from 'vue'
+/* =========================
+   LOAD ENTIDADE
+========================= */
+async function loadEntidade(id) {
+  if (!id) return
 
-export default defineComponent({
-  name: 'Logo',
-  components: {
+  loading.value = true
 
-  },
-  
-  computed: {
-  },
-  watch: {
-    Entidade (value, oldvalue) {
-    }
-  },
-  methods: {
-    getHostname (tipoEnt) {
-      const domain = window.location.href.split('/')[2].split('.')[0]
-      if (domain.toLocaleLowerCase() !== tipoEnt.nome.toLowerCase()) {
-        return true
-      } else {
-        this.$store.commit('tipoEntidade/SET_TIPO_ENTIDADE', tipoEnt)
-        this.TipoEntidade = tipoEnt
-        setStorage('c', 'tipoEntidade', JSON.stringify(tipoEnt))
-        return false
-      }
-    },
+  try {
+    const { data } = await HTTPClient.get(
+      url({ type: 'u', url: 'entidades/' + id })
+    )
 
-    async getTipoEntidades () {
-      this.spiner = true
-      try {
-        await HTTPClient.get(apiUrl({ type: 'au', url: 'tipoEntidades/', params: {} }))
-          .then(res => {
-            this.$store.commit('tipoEntidade/SET_TIPO_ENTIDADES', res?.data?.results)
-            res?.data?.results.forEach(tipoEntidade => {
-              this.getHostname(tipoEntidade)
-            })
-            this.spiner = false
-          }).catch(err => {
-            this.spiner = false
-            console.log(err)
-          })
-      } catch (error) {
-        console.log(error.message)
-      }
-    },
+    User.Entidade = data
+  } catch (err) {
+    console.error('Erro ao carregar entidade:', err)
+  } finally {
+    loading.value = false
+  }
+}
 
-    async getEntidade () {
-      try {
-        await HTTPClient.get(url({ type: 'u', url: 'entidades/' + this.$route.params.entidade_id, params: {} }))
-          .then(res => {
-            this.$store.commit('entidade/SET_ENTIDADE', res?.data)
-            setStorage('c', 'entidade', JSON.stringify(res?.data))
-            this.Entidade = JSON.parse(getStorage('c', 'entidade'))
-            this.entidadeLogo = res?.data?.logo?.url
-            this.spiner = false
-          }).catch(err => {
-            this.spiner = false
-            console.log(err)
-          })
-      } catch (error) {
-        console.log(error.message)
-      }
-    }
-  },
-
-  mounted () {
-    if (this.$route.params.entidade_id) {
-      this.getEntidade(this.$route.params.entidade_id)
-    } else {
-      this.getTipoEntidades()
-    }
+/* =========================
+   AUTO LOAD ON MOUNT
+========================= */
+onMounted(() => {
+  if (route.params.entidade_id) {
+    loadEntidade(route.params.entidade_id)
   }
 })
 
+/* =========================
+   AUTO RELOAD WHEN ROUTE CHANGES
+========================= */
+watch(
+  () => route.params.entidade_id,
+  (newId, oldId) => {
+    if (newId && newId !== oldId) {
+      loadEntidade(newId)
+    }
+  }
+)
 </script>
